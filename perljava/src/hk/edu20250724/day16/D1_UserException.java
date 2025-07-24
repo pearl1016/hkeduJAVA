fa9bd3887eeab016b2a4aa1cd1cb1266f2a1d848
Pearl1016 <114461580+pearl1016@users.noreply.github.com>
2025-07-24 17:18:57 +0900

    Update D1_UserException.java

--- perljava/src/hk/edu20250724/day16/D1_UserException.java
+++ perljava/src/hk/edu20250724/day16/D1_UserException.java
@@ -1,16 +1,15 @@
-//package hk.edu20250724.day16;
-//
-////내가 필요한 Exception클래스를 정의하려면 Exception클래스를 상속받아야한다.
-//public class D1_UserException extends Exception {
-//
-//	private static final long serialVersionUID = 1L;
-//	
-//	public D1_UserExceptionTest() {
-//		this("UserException 오류입니다.");
-//	}
-//	
-//	public D1_UserExceptionTest(String msg) {
-//		super(msg); //super로 전달
-//	}
-//	
-//}
+package hk.edu20250724.day16;
+
+//내가 필요한 Exception 클래스를 정의하려면 Exception클래스를 상속받아야 한다.
+public class D1_UserException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public D1_UserException() {
+		this("UserException 오류입니다.");
+	}
+	
+	public D1_UserException(String msg) {
+		super(msg);
+	}
+}
